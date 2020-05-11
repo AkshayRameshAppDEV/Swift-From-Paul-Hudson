676f7177856d4466e51aebc5d9551e2039210cc0
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-05-11 16:31:34 -0500

    Incorporated Assets + Auto Layout

---
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by Akshay Ramesh on 5/11/20.
+//  Copyright © 2020 Akshay Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
