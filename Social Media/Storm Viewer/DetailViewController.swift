978afdf4e2e80213ec11cd8af76cb746bcbc4d40
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-05-11 21:08:29 -0500

    Social Media Share icon

---
+++ Social Media/Storm Viewer/DetailViewController.swift
@@ -0,0 +1,59 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Akshay Ramesh on 5/10/20.
+//  Copyright © 2020 Akshay Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var currentImagePosition: Int?
+    var totalNumberOfImages: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "\(currentImagePosition!) of \(totalNumberOfImages!)"
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage{
+            imageView.image = UIImage(named: imageToLoad)
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped(){
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8)
+            else{
+            print("No image found")
+                return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Social Media/Storm Viewer/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Akshay Ramesh on 5/10/20.
+//  Copyright © 2020 Akshay Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items{
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        sortImageNames()
+        print(pictures.sorted())
+    }
+    
+    // Number of Rows in section in table
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    // populate each cell with image name from the pictures array
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let viewController = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController{
+            viewController.selectedImage = pictures[indexPath.row]
+            viewController.currentImagePosition = indexPath.row + 1
+            viewController.totalNumberOfImages = String(pictures.count)
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+    
+    func sortImageNames() {
+        self.pictures = pictures.sorted()
+    }
+
+
+}
+
